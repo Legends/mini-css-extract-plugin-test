f8671303ab8055acfb340abeb11c4ab78d6ecb49
Legends <jovan27@gmx.de>
2018-05-16 17:06:10 +0200

    fdsafas

--- src/entry.js
+++ src/entry.js
@@ -1,21 +1,29 @@
 //  import * as $ from "jquery"
 // import * as myapp from "./myapp"
 let myapp = require("./myapp");
- 
- var $ = require("jquery");
 
- // legends
+var $ = require("jquery");
+var x = 4 % 2;
+if (x === 0) {
+    require("acorn");
+}
+
+// legends
 var $body = $("body");
 var $p = $("<p>hi</p>");
 
 $body.append($p);
 
- try {
-     
-    let a = new myapp(11,"Johan");
+try {
+
+    let a = new myapp(11, "Johan");
     a.say();
-    var p =  a.test();
-    p.then(()=>{$p.text("success");}).catch(()=>{$p.text("fail");});
- } catch (e) {
-     console.error(e);
- }
+    var p = a.test();
+    p.then(() => {
+        $p.text("success");
+    }).catch(() => {
+        $p.text("fail");
+    });
+} catch (e) {
+    console.error(e);
+}
